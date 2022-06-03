748763e04806fa214dfed6b0f4a2013f5122e201
Marina Ivantsova <marineivantsova@yandex.ru>
2022-06-03 06:20:04 +0300

    Logical operators

---
+++ logicalOperators/codeOutput.js
@@ -0,0 +1,7 @@
+// Что выведет код ниже?
+
+// alert( null || 2 || undefined ); // 2
+// alert( alert(1) || 2 || alert(3) ); // Сначала 1, потом 2
+// alert( 1 && null && 2 ); // null
+// alert( alert(1) && alert(2) ); // Сначала 1, затем undefined
+// alert( null || 2 && 3 || 4 ); // 3
