60dfcbcd8f5a6358be24ec3d9ca2a7a24ab908e2
Marina Ivantsova <marineivantsova@yandex.ru>
2022-06-08 18:58:26 +0300

    Функция оптимизирована

--- functions/pow.js
+++ functions/pow.js
@@ -8,23 +8,22 @@
 
 // P.S. В этой задаче функция обязана поддерживать только натуральные значения n, т.е. целые от 1 и выше.
 
-
-
-function pow(x, n) {
+let pow = function (x, n) {
+    if (n < 1 || (n % 1 !== 0)) {
+        return false;
+    }
     let result = x;
     for (let i = 1; i < n; i++) {
         result = result * x;
     }
     return result;
-
 }
-let x = prompt("Введите x:", '');
-let n = prompt("Введите n:", '');
+let x = +prompt("Введите x:", '');
+let n = +prompt("Введите n:", '');
+const res = pow(x, n);
 
-if (n < 1) {
-    console.log(`Степень ${n} не поддерживается, используйте натуральное число`);
+if (res === false) {
+    console.log(`Степень ${n} не поддерживается, используйте натуральное и целое число`);
 } else {
-    console.log(pow(x, n));
+    console.log(res)
 }
-
-console.log(pow(x, n));
