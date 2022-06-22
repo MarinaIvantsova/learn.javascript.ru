64b57a7a6a09e5c2d5f805ba207ac70fc0d2d346
Marina Ivantsova <marineivantsova@yandex.ru>
2022-06-22 14:29:14 +0300

    Методы массивов

--- arraysMethods/changeLetters.js
+++ arraysMethods/changeLetters.js
@@ -1,7 +1,6 @@
 /*Переведите текст вида border-left-width в borderLeftWidth
-важность: 5
-Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
 
+Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
 То есть дефисы удаляются, а все слова после них получают заглавную букву.
 
 Примеры:
@@ -9,16 +8,32 @@
 camelize("background-color") == 'backgroundColor';
 camelize("list-style-image") == 'listStyleImage';
 camelize("-webkit-transition") == 'WebkitTransition';
-P.S. Подсказка: используйте split, чтобы разбить строку на массив символов,
- потом переделайте всё как нужно и методом join соедините обратно.*/
 
- let camelize = (str) => {
-   let splitted = str.endsWith('-') ? str.slice(0, -1).split('') : str.split('');
-    splitted.forEach((element, index) => {
-      if (element.includes("-")) {
-         splitted.splice(index, 2, splitted[index + 1].toUpperCase());
-      } 
-    });
-    return splitted.join("");
- }
- console.log(camelize("-webkit-transition-"));
+Decision №1.
+
+let camelize = (str) => {
+  let splitted = str.endsWith('-') ? str.slice(0, -1).split('') : str.split('');
+  let strJoin = splitted.join('');
+
+  splitted.forEach((element, index) => {
+    if (element === "-") {
+      splitted.splice(index, 2, splitted[index + 1].toUpperCase());
+      strJoin = splitted.join('');
+    }
+  });
+
+  return strJoin;
+}
+
+console.log(camelize("le-id"));
+*/
+
+// Decision №2.
+function camelize(str) {
+  let splitted = str.endsWith('-') ? str.slice(0, -1).split('-') : str.split('-');
+  return splitted
+    .map((element, index) => index === 0 ? element : element[0].toUpperCase() + element.slice(1))
+    .join('');
+}
+
+console.log(camelize("le-im-"));
