8d7aee6d7927dd7b05fb570037eb72918f1d5cf4
Marina Ivantsova <marineivantsova@yandex.ru>
2022-06-28 10:02:54 +0300

    Strings tasks

---
+++ strings/spamCheck.js
@@ -0,0 +1,18 @@
+/*
+Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
+
+Функция должна быть нечувствительна к регистру:
+
+checkSpam('buy ViAgRA now') == true
+checkSpam('free xxxxx') == true
+checkSpam("innocent rabbit") == false
+*/
+function checkSpam(str) {
+  let lower = str.toLowerCase();
+
+  return  str.toLowerCase().includes('viagra') || lower.includes('xxx');    
+}
+
+console.log(checkSpam('buy ViAgRA now'));
+console.log(checkSpam('free xxxxx'));
+console.log(checkSpam("innocent rabbit"));
