ecaea34adfddc3c7077174e8b2f5f0d15e4d2f1b
Marina Ivantsova <marineivantsova@yandex.ru>
2022-06-30 13:40:50 +0300

    Задачи на методы массивов

---
+++ arraysMethods/uniqueElements.js
@@ -0,0 +1,24 @@
+/*
+Пусть arr – массив строк.
+
+Напишите функцию unique(arr), которая возвращает массив, содержащий только уникальные элементы arr.
+
+*/
+
+function unique(arr) {
+    let result = [];
+
+    for (let elem of arr) {
+        if (!result.includes(elem)) {
+            result.push(elem);
+        }
+    }
+    
+    return result;
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+    "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+console.log(unique(strings)); // кришна, харе, :-O
