3912425d352082207bfc4b5cd078f1b543b74514
Marina Ivantsova <marineivantsova@yandex.ru>
2022-07-07 05:54:59 +0300

    Задача на Форматирование относительной даты

--- date/formatting.js
+++ date/formatting.js
@@ -19,6 +19,44 @@ alert( formatDate(new Date(new Date - 86400 * 1000)) );
 */
 
 function formatDate(date) {
-    
+
+  let diff = new Date() - date;
+
+  if (diff < 1000) {
+    return 'прямо сейчас';
   }
-console.log(formatDate(new Date(new Date - 3600 * 1000)));
+
+  let sec = Math.floor(diff / 1000);
+
+  if (sec < 60) {
+    return `${sec} секунд назад`;
+  }
+
+  let min = sec / 60;
+
+  if (min < 60) {
+    return `${min} минут назад`;
+  }
+
+  let arr = [
+    '0' + date.getDate(),
+    '0' + (date.getMonth() + 1),
+    '' + date.getFullYear(),
+    '0' + date.getHours(),
+    '0' + date.getMinutes(),
+  ];
+
+  let mapped = arr.map((elem) => {
+    return elem.slice(-2);
+  });
+
+  return mapped.slice(0, 3).join('.') + ' ' + mapped.slice(3).join(':');
+}
+
+console.log(formatDate(new Date(new Date - 1))); // "прямо сейчас"
+
+console.log(formatDate(new Date(new Date - 30 * 1000))); // "30 сек. назад"
+
+console.log(formatDate(new Date(new Date - 5 * 60 * 1000))); // "5 мин. назад"
+
+console.log(formatDate(new Date(new Date - 86400 * 1000)));
