5f3d7f19bdddf652bb473cf5a3f4dae17b0726d3
Marina Ivantsova <marineivantsova@yandex.ru>
2022-07-29 13:55:58 +0300

    4я задача на Замыкания(Exercism)

--- exercismTasks/closure.js
+++ exercismTasks/closure.js
@@ -6,9 +6,9 @@
 // result => [6, 8]
 
 function translate2d(dx, dy) {
-    return function (x, y) {
-       return [x + dx, y + dy];
-     };
+  return function (x, y) {
+    return [x + dx, y + dy];
+  };
 }
 
 // Task 2. Implement the scale2d function
@@ -21,10 +21,10 @@ function translate2d(dx, dy) {
 // result => [12, -6]
 
 function scale2d(sx, sy) {
-    return function (x, y) {
-     return [x * sx, y * sy];
-   };
- }
+  return function (x, y) {
+    return [x * sx, y * sy];
+  };
+}
 
 
 //  Task 3. Combine two transformation functions to perform a repeatable transformation.
@@ -40,30 +40,65 @@ function scale2d(sx, sy) {
 // const result = composedTransformations(0, 1);
 // result => [4, 2]
 
- function translate2d(dx, dy) {
-    return function (x, y) {
-       return [x + dx, y + dy];
-     };
-   }
-  
-   function scale2d(sx, sy) {
-    return function (x, y) {
-     return [x * sx, y * sy];
-   };
+function translate2d(dx, dy) {
+  return function (x, y) {
+    return [x + dx, y + dy];
+  };
+}
+
+function scale2d(sx, sy) {
+  return function (x, y) {
+    return [x * sx, y * sy];
+  };
+}
+function composeTransform(f, g) {
+  return function composed(x, y) {
+    return g(...f(x, y))
   }
-  function composeTransform(f, g) {
-    return function composed(x, y) {
-      return g(...f(x, y))
+}
+
+
+const moveCoordinatesRight2Px = translate2d(2, 0);
+const doubleCoordinates = scale2d(2, 2);
+const composedTransformations = composeTransform(
+  moveCoordinatesRight2Px,
+  doubleCoordinates
+);
+const result = composedTransformations(0, 1);
+
+console.log(result);
+
+//Task 4. 
+//   Implement the memoizeTransform function.
+// It takes a function to memoize, then returns a new function that remembers the inputs to the supplied function
+// so that the last return value can be "remembered" and only calculated once if it is called again with the same arguments.
+
+// Memoizing is sometimes used in dynamic programming.
+// It allows for expensive operations to be done only once since their results are remembered.
+// Note that in this exercise only the last result is remembered,
+// unlike some solutions in dynamic programming that memoize all results.
+
+function scale2d(sx, sy) {
+  return function (x, y) {
+    return [x * sx, y * sy];
+  };
+}
+
+function memoizeTransform(f) {
+  let save, savedX, savedY;
+  return function (x, y) {
+    if (savedX !== x || savedY !== y) {
+      save = f(x, y);
+      savedX = x;
+      savedY = y;
     }
-  }
-  
-  
-  const moveCoordinatesRight2Px = translate2d(2, 0);
-  const doubleCoordinates = scale2d(2, 2);
-  const composedTransformations = composeTransform(
-    moveCoordinatesRight2Px,
-    doubleCoordinates
-  );
-  const result = composedTransformations(0, 1);
-  
-  console.log(result);
+
+    return save;
+  };
+}
+
+const tripleScale = scale2d(3, 3);
+const memoizedScale = memoizeTransform(tripleScale);
+
+console.log(memoizedScale(4, 3)); // => [12, 9], this is computed since it hasn't been computed before for the arguments
+console.log(memoizedScale(4, 3)); // => [12, 9], this is remembered, since it was computed already
