382b4bd782a0b1928e7f347d217121376a45c706
Marina Ivantsova <marineivantsova@yandex.ru>
2022-10-17 11:11:08 +0300

    Задачи на async/await

--- async-await/showAvatar.js
+++ async-await/showAvatar.js
@@ -1,7 +1,25 @@
 async function showAvatar(url) {
+
 // запрашиваем JSON с данными пользователя
 let response = await fetch('../promiseChain/github.json');
 let user = await response.json();
+
+// запрашиваем информацию об этом пользователе из github
+let githubResponse = await fetch(`https://api.github.com/users/${user.login}`);
+let gitHubUser = await githubResponse.json();
+
+// отображаем аватар пользователя
+let img = document.createElement('img');
+img.src = gitHubUser.avatar_url;
+img.className = "promise-avatar-example";
+document.body.append(img);
+
+// ждём 3 секунды и затем скрываем аватар
+await new Promise((resolve) => setTimeout(resolve, 3000));
+
+img.remove();
+
+return gitHubUser;
 }
 
 showAvatar();
