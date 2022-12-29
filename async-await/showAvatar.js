98d5f599744eb352e90715287d6a2357ec51b825
Marina Ivantsova <marineivantsova@yandex.ru>
2022-12-29 14:11:08 +0300

    async/await

--- async-await/showAvatar.js
+++ async-await/showAvatar.js
@@ -1,25 +1,24 @@
 async function showAvatar(url) {
+	// запрашиваем JSON с данными пользователя
+	let response = await fetch('../promiseChain/github.json');
+	let user = await response.json();
 
-// запрашиваем JSON с данными пользователя
-let response = await fetch('../promiseChain/github.json');
-let user = await response.json();
+	// запрашиваем информацию об этом пользователе из github
+	let githubResponse = await fetch(`https://api.github.com/users/${user.login}`);
+	let gitHubUser = await githubResponse.json();
 
-// запрашиваем информацию об этом пользователе из github
-let githubResponse = await fetch(`https://api.github.com/users/${user.login}`);
-let gitHubUser = await githubResponse.json();
+	// отображаем аватар пользователя
+	let img = document.createElement('img');
+	img.src = gitHubUser.avatar_url;
+	img.className = 'promise-avatar-example';
+	document.body.append(img);
 
-// отображаем аватар пользователя
-let img = document.createElement('img');
-img.src = gitHubUser.avatar_url;
-img.className = "promise-avatar-example";
-document.body.append(img);
+	// ждём 3 секунды и затем скрываем аватар
+	await new Promise(resolve => setTimeout(resolve, 3000));
 
-// ждём 3 секунды и затем скрываем аватар
-await new Promise((resolve) => setTimeout(resolve, 3000));
+	img.remove();
 
-img.remove();
-
-return gitHubUser;
+	return gitHubUser;
 }
 
 showAvatar();
